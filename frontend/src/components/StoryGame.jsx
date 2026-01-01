32abcb9177cf7ac0e953674ffd2224183efe0034
Abuaisha-7 <heyrubedru8086@gmail.com>
2026-01-01 15:17:14 +0300

    back to local db

--- frontend/src/components/StoryGame.jsx
+++ frontend/src/components/StoryGame.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const StoryGame = ({story, onNewStory}) => {
   const [currentNodeId, setCurrentNodeId] = useState(null)
